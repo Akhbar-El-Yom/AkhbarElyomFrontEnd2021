a11ccb5ed5f5f5ed9deb477580ad772ac48f9b32
Ahmed Hassan <eng.a7mad7asan@hotmail.com>
2020-12-17 19:23:51 +0200

    Create Section Page

---
+++ akhbarelyom.front.end.2021/MVC/Services/EFQueries/Section.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Services.EFQueries
+{
+    public class Section
+    {
+        private readonly AppDbContext _db;
+        public Section(AppDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        public MainSection GetSectionItem(int SectionID) => _db.MainSections
+                .Include(e => e.News.OrderByDescending(e => e.AddedDate)) // 
+                    .ThenInclude(e => e.Picture1)
+                .Where(e => e.SectionId == SectionID).Single();
+
+        public List<News> GetSectionItem2(int SectionID) => _db.News
+                .Include(e => e.Section) // 
+                .Include(e => e.Picture1)
+                .Where(e => e.SectionId == SectionID).Take(15).ToList();
+    }
+}
