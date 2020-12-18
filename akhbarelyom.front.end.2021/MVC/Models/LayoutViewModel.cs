789ac4081f6f98e43c1002f7bbabcbd5b8ba7a29
Ahmed Galal <ahmedgalal007@hotmail.com>
2020-12-18 23:08:11 +0200

    Done: Create The _MegaMenuCarouselOWL Mega Menu Slider

---
+++ akhbarelyom.front.end.2021/MVC/Models/LayoutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class LayoutViewModel
+    {
+    }
+
+    public class MegaMenuCarouselOwlViewModel
+    {
+        public MainSection MainSection { get; set; }
+
+    }
+}
